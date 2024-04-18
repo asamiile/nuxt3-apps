e184878cea5dac364b2c4035a6ff167de9008499
asamiile <asamin47@gmail.com>
2024-04-18 21:57:36 +0900

    fixed.

--- pages/sample/vue/vue-api/02_composition-api/computed.vue
+++ pages/sample/vue/vue-api/02_composition-api/computed.vue
@@ -1,13 +1,13 @@
-<script lang="ts">
+<script setup lang="ts">
 // https://ja.vuejs.org/api/reactivity-core.html
 
-// import { ref } from 'vue'
+import { ref } from 'vue'
 
-// // 読み取り専用の算出 ref の作成:
-// const count = ref(1)
-// const plusOne = computed(() => count.value + 1)
+// 読み取り専用の算出 ref の作成:
+const count = ref(1)
+const plusOne = computed(() => count.value + 1)
 
-// console.log(plusOne.value) // 2
+console.log(plusOne.value) // 2
 
 // plusOne.value++ // エラー
 
