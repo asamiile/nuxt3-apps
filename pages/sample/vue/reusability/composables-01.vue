7b80e82a6d7c2f2cf624f3954c86b09bd56bd678
asamiile <asamin47@gmail.com>
2024-04-18 22:31:25 +0900

    add vue samples.

---
+++ pages/sample/vue/reusability/composables-01.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useMouse } from '~/composables/useMouse.ts'
+
+const { x, y } = useMouse()
+
+// const mouse = reactive(useMouse())
+// // mouse.x は元の ref とリンクされる
+// console.log(mouse.x)
+</script>
+
+<template>Mouse position is at: {{ x }}, {{ y }}</template>
---
+++ pages/sample/vue/reusability/custom-directives.vue
@@ -0,0 +1,10 @@
+<script setup>
+// テンプレート内で v-focus が有効になります
+const vFocus = {
+  mounted: (el) => el.focus()
+}
+</script>
+
+<template>
+  <input v-focus />
+</template>
