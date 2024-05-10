ac94339342dd6273eb7e2cad2ef55d06c82d9e17
asamiile <asamin47@gmail.com>
2024-05-10 19:03:27 +0900

    add pinia.

---
+++ pages/sample/pinia/01_introduction.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/sample/counter'
+import { storeToRefs } from 'pinia'
+
+const counter = useCounterStore()
+
+// counter.count++
+// // with autocompletion ✨
+// counter.$patch({ count: counter.count + 1 })
+// // or using an action instead
+// counter.increment()
+
+const { name, doubleCount } = storeToRefs(store)
+const { increment } = store
+</script>
+
+<template>
+  <!-- Access the state directly from the store -->
+  <div>Current Count: {{ counter.count }}</div>
+</template>
